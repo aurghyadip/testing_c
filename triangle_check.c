b2d91e090368e9d7fc993aa5727f7530cc564c40
Aurghyadip <adkundu@gmail.com>
2017-03-13 18:36:18 +0530

    Added functions

--- triangle_check.c
+++ triangle_check.c
@@ -1,32 +1,49 @@
-/*
- * C program to check whether a triangle is Equilateral, Isosceles or Scalene
-*/
- 
+#include <crest.h>
 #include <stdio.h>
- 
+
+void triangle_check(int, int, int);
+void triangle_type(int, int, int);
+
 int main()
 {
-    int a, b, c; //a, b, c are three sides of a triangle
- 
-    /* Reads all sides of a triangle */
-    printf("Enter three sides of triangle: ");
-    scanf("%d%d%d", &a, &b, &c);
- 
-    if(a==b && b==c) 
-    {
-        //If all sides are equal
-        printf("Equilateral triangle.\n");
-    }
-    else if(a==b || a==c || b==c) 
-    {
-        //If two sides are equal
-        printf("Isosceles triangle.\n");
-    }
-    else
-    {
-        //If none sides are equal
-        printf("Scalene triangle.\n");
-    }
- 
-    return 0;
-} 
+  int a, b, c;
+  CREST_int(a);
+  CREST_int(b);
+  CREST_int(c);
+  triangle_check(a,b,c);
+  return 0;
+}
+
+void triangle_check(int a, int b, int c)
+{
+	if (a <= 0 || b <= 0 || c <= 0)
+	{
+		printf("%s\n", "Not a valid traingle");
+	}
+	else
+	{
+		if ((a+b)>c || (a+c)>b || (b+c) > a)
+      triangle_type(a,b,c);
+	  else
+      printf("Not a valid triangle!\n");
+	}
+}
+
+void triangle_type(int a, int b, int c)
+{
+  if(a==b && b==c)
+  {
+    printf("a = %d\nb = %d\nc = %d\n",a,b,c);
+    printf("Equilateral triangle.\n");
+  }
+  else if(a==b || a==c || b==c)
+  {
+    printf("a = %d\nb = %d\nc = %d\n",a,b,c);
+    printf("Isosceles triangle.\n");
+  }
+  else
+  {
+    printf("a = %d\nb = %d\nc = %d\n",a,b,c);
+    printf("Scalene triangle.\n");
+  }
+}
